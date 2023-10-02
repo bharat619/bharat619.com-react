18a1a3f42030f751b3df464b613b4e7b9a9ad53d
Bharat <baru.hegde@gmail.com>
2023-10-02 10:57:02 +0530

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+import Testimonial from "./components/testimonial/Testimonial";
+
+const App = () => {
+  return (
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <About></About>
+      <Experience></Experience>
+      <Services></Services>
+      <Portfolio></Portfolio>
+      <Testimonial></Testimonial>
+      <Contact></Contact>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default App;
