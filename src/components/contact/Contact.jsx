cfdbcb783612ce9197e663a9a9b348d5601277a3
Bharat <baru.hegde@gmail.com>
2023-10-02 17:51:12 +0530

    Finish raw portfolio

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,94 @@
+import { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiMessengerFill } from "react-icons/ri";
+import { BsWhatsapp } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+  const [disabled, setDisabled] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    setDisabled(true);
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAIL_JS_SERVICE_ID,
+        process.env.REACT_APP_EMAIL_JS_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAIL_JS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          e.target.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      )
+      .finally(() => {
+        setDisabled(false);
+      });
+  };
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>bharathegde619@gmail.com</h5>
+            <a href="mailto:bharathegde619@gmail.com">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            <RiMessengerFill className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>bharat.hegde.750</h5>
+            <a href="https://m.me/bharat.hegde.750">Send a message</a>
+          </article>
+        </div>
+
+        <form ref={form} onSubmit={sendEmail} aria-disabled>
+          <fieldset disabled={disabled}>
+            <input
+              type="text"
+              name="name"
+              placeholder="Your Full Name"
+              required
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              required
+            />
+
+            <textarea
+              name="message"
+              rows="7"
+              placeholder="Your Message"
+              required
+            />
+
+            <button
+              disabled={disabled}
+              type="submit"
+              className="btn btn-primary"
+            >
+              {disabled ? "Sending...." : "Send Message"}
+            </button>
+          </fieldset>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,13 +5,13 @@ import { AiFillMediumCircle } from "react-icons/ai";
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
-      <a href="https://linkedin.com" target="_blank">
+      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
         <BsLinkedin />
       </a>
-      <a href="https://github.com" target="_blank">
+      <a href="https://github.com" target="_blank" rel="noreferrer">
         <FaGithub />
       </a>
-      <a href="https://medium.com" target="_blank">
+      <a href="https://medium.com" target="_blank" rel="noreferrer">
         <AiFillMediumCircle />
       </a>
     </div>
