588ab0341bac101448e4119ffe63b7b1e0b68ea5
Bharat <baru.hegde@gmail.com>
2023-10-02 16:05:50 +0530

    Add more components and swiper

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -1,7 +1,60 @@
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
 import "./testimonial.css";
+import AVTR1 from "../../assets/avatar1.jpg";
 
 const Testimonial = () => {
-  return <section id="testimonials">Testimonial</section>;
+  const data = [
+    {
+      avatar: AVTR1,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam voluptatibus eos molestias minus vitae ex modi laboriosam doloremque ducimus ullam necessitatibus rerum soluta ut nihil accusantium nemo et, suscipit impedit?",
+      name: "Tina Snow",
+      id: 1,
+    },
+    {
+      avatar: AVTR1,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam voluptatibus eos molestias minus vitae ex modi laboriosam doloremque ducimus ullam necessitatibus rerum soluta ut nihil accusantium nemo et, suscipit impedit?",
+      name: "Tina Snow",
+      id: 2,
+    },
+    {
+      avatar: AVTR1,
+      review:
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam voluptatibus eos molestias minus vitae ex modi laboriosam doloremque ducimus ullam necessitatibus rerum soluta ut nihil accusantium nemo et, suscipit impedit?",
+      name: "Tina Snow",
+      id: 3,
+    },
+  ];
+
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        className="container testimonials__container"
+        spaceBetween={40}
+        slidesPerView={1}
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+      >
+        {data.map((d) => (
+          <SwiperSlide className="testimonial">
+            <div className="client__avatar">
+              <img src={d.avatar} alt="" />
+            </div>
+            <h5>{d.name}</h5>
+            <small className="client__review">{d.review}</small>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonial;
